ff1e2d6fbc8aff749d7d5d58aa0e6da04e9b441f
sravan1821 <sravanrock1821@gmail.com>
2026-01-31 14:23:28 +0530

    Update Login.jsx

--- firstapp/src/comonents/Login.jsx
+++ firstapp/src/comonents/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
 import axios from "axios"
-import "./Login.css"
+import "./login.css"
 
 export default function Login() {
   const [email, setEmail] = useState("")
